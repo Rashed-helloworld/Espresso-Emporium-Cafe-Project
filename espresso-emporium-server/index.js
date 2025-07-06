74b564242a8a7e0ccb116d3c4b93be31e4e9aef4
Rashed-helloworld <rashed.helloworld@gmail.com>
2025-07-06 07:24:15 +0600

    daisyUI added and server setup successfully

--- espresso-emporium-server/index.js
+++ espresso-emporium-server/index.js
@@ -1,1 +1,16 @@
- 
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 3000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Cafe Espresso Emporium");
+});
+
+app.listen(port, () => {
+    console.log(`port is: ${port}`);
+})
